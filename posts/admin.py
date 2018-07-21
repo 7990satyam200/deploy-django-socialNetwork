e00230c4bccf4d4cfbc69698f3f1039bc7c13517
7990satyam200 <7990satyam200@gmail.com>
2018-07-21 20:06:28 +0530

    First version of application moved into github

---
+++ posts/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+
+from . import models
+class CommentInline(admin.StackedInline):
+    model = models.Comment
+
+
+class PostAdmin(admin.ModelAdmin):
+    inlines = [
+        CommentInline,
+    ]
+admin.site.register(models.Post, PostAdmin)
+admin.site.register(models.Comment)
---
+++ posts/views.py
@@ -0,0 +1,42 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+
+from . import models
+
+
+class PostListView(LoginRequiredMixin, ListView): # new
+    model = models.Post
+    template_name = 'post_list.html'
+    login_url = 'login' # new
+
+
+class PostCreateView(LoginRequiredMixin, CreateView): # new
+    model = models.Post
+    template_name = 'post_new.html'
+    fields = ['message']
+    login_url = 'login' # new
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+
+class PostDetailView(LoginRequiredMixin, DetailView): # new
+    model = models.Post
+    template_name = 'post_detail.html'
+    login_url = 'login' # new
+
+
+class PostUpdateView(LoginRequiredMixin, UpdateView): # new
+    model = models.Post
+    fields = ['message']
+    template_name = 'post_edit.html'
+    login_url = 'login' # new
+
+class PostDeleteView(LoginRequiredMixin, DeleteView): # new
+    model = models.Post
+    template_name = 'post_delete.html'
+    success_url = reverse_lazy('posts')
+    login_url = 'login' # new
